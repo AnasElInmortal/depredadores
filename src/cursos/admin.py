0f71f94834971dfe95fd902b8e756111dae1b2c6
AnasElInmortal <AnasElInmortal@users.noreply.github.com>
2017-08-08 12:57:54 -0500

    Add files via upload

--- src/cursos/admin.py
+++ src/cursos/admin.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+
 from django.contrib import admin
+from .models import TipoCurso
+from .models import Curso
+
 
 # Register your models here.
+class cursosField(admin.ModelAdmin):
+    list_display = ('idTipoCurso', 'cursoCode', 'cursoVideo', 'idCinta',
+                    'descripcion',)
+    list_filter = ['idTipoCurso__tipoCursoCode', 'idCinta__cintaCode']
+    search_fields = ['idTipoCurso__tipoCursoCode', 'cursoCode', 'idCinta__cintaCode' ]
+
+
+admin.site.register(TipoCurso)
+admin.site.register(Curso,cursosField)
